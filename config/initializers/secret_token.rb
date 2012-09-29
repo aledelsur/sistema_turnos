f97d92931c3ba8f6ac99ea356d04854a3018f79c
Ale - aledelsur <alealvarez_00@hotmail.com>
2012-09-29 12:12:24 -0300

    first commIT

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SistemaTurnos::Application.config.secret_token = '904371816a2894f0c9080ab90cba1f0807d591c2af39d3680dc9144c31ba6a54d158947b41d5fa117c8a30efcd33d89125dc4c98fec05f5fb6faee28a4b7c1b6'
